1c2eb655888fbcb79b50cde1c3c88256bc97f7e7
ekrem0691 <06hilalekrem06@gmail.com>
2023-11-20 23:17:01 +0300

    second commit

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,6 +1,19 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route('/')
+def head():
+    return "hello world"
+
+@app.route('/second')
+def second():
+    return "this is second page"
+
+@app.route('/third')
+def third():
+    return "this is third page"
+
+ 
 
 
 
@@ -9,4 +22,4 @@ app = Flask(__name__)
 if __name__ == '__main__':
 
     app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    # app.run(host= '0.0.0.0', port=8081) EC2 Run Port and host preview.
